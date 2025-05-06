6d8bf626c3cb6fe4a0cd95b538e0fc72ec1ecc33
yoko bessho <yoko.murakami.11.18@gmail.com>
2025-05-06 17:21:37 +0900

    ダミー入れる

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Contact;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,9 +12,14 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
+    // public function run()
+    // {
+    //     $this->call(CategoriesTableSeeder::class);
+    // }
+
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-        $this->call(CategoriesTableSeeder::class);
+        $this->call(ContactsTableSeeder::class);
+
     }
 }
